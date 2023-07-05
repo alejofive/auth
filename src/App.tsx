6871cbcb2f25d474514ba7876c3037447405831f
Alejandro Garcia <alejofive555@gmail.com>
2023-07-05 15:40:46 -0500

    provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { AuthContext } from "../src/context";
+import { AuthContext, Provider } from "../src/context";
 import "./App.css";
 import Dashboard from "./Pages/Dashboard";
 import Login from "./Pages/Login";
@@ -11,21 +11,23 @@ function App() {
   console.log(auth);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        {!auth ? (
-          <>
-            <Route path="/" Component={() => <Login />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </>
-        ) : (
-          <>
-            <Route path="/dashboard" Component={() => <Dashboard />} />
-            <Route path="*" element={<Navigate to="/dashboard" replace />} />
-          </>
-        )}
-      </Routes>
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <Routes>
+          {!auth ? (
+            <>
+              <Route path="/" Component={() => <Login />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </>
+          ) : (
+            <>
+              <Route path="/dashboard" Component={() => <Dashboard />} />
+              <Route path="*" element={<Navigate to="/dashboard" replace />} />
+            </>
+          )}
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 export default App;
