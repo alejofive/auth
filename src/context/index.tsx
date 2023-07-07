ee51885920570f87ce2efd6aeb96b930009cf7e0
Luis Fernandez <luisdk.03@gmail.com>
2023-07-07 08:31:51 -0500

    feat: localStorage

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,35 +1,32 @@
-import { createContext, useState } from "react";
+import { createContext, useState } from 'react'
 
 type AuthType = {
-  token: string;
+  token: string
   user: {
-    id: string;
-    name: string;
-  };
-};
+    id: string
+    name: string
+  }
+}
 
 type ContextAuthType = {
-  auth: AuthType;
-  changeAuth: (params: AuthType) => void;
-};
-export const AuthContext = createContext<ContextAuthType | null>(null);
+  auth: AuthType
+  changeAuth: (params: AuthType) => void
+}
+export const AuthContext = createContext<ContextAuthType | null>(null)
 
 export const Provider = ({ children }: { children: any }) => {
-  const localAuthData = JSON.parse(localStorage.getItem("auth") || "");
-  console.log(localAuthData);
+  const localAuthData = localStorage.getItem('auth')
+    ? JSON.parse(localStorage.getItem('auth') || '')
+    : {
+        token: '',
+        user: {
+          id: '',
+          name: '',
+        },
+      }
 
-  const [auth, setAuth] = useState(
-    localAuthData
-      ? localAuthData
-      : {
-          token: "",
-          user: {
-            id: "",
-            name: "",
-          },
-        }
-  );
-  const changeAuth = (params: AuthType) => [setAuth(params)];
+  const [auth, setAuth] = useState(localAuthData)
+  const changeAuth = (params: AuthType) => [setAuth(params)]
 
   return (
     <AuthContext.Provider
@@ -40,5 +37,5 @@ export const Provider = ({ children }: { children: any }) => {
     >
       {children}
     </AuthContext.Provider>
-  );
-};
+  )
+}
